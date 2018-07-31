6b102acc01662f1aa8b3408f584317817781e4a0
Yusuke Izawa <yuizalp@gmail.com>
2018-07-31 11:42:19 +0900

    figure out running time

--- src/main.ml
+++ src/main.ml
@@ -2,6 +2,7 @@ open Dpll
 
 let () =
   let ic = open_in (Sys.argv.(1)) in
+  let start = Unix.gettimeofday () in
   try
     (match
       Lexing.from_channel ic
@@ -9,7 +10,10 @@ let () =
       |> Cnf.literal_of_lists
       |> Solver.solve
      with
-     | Solver.Sat _ -> print_endline "SAT"
-     | Solver.Unsat _ -> print_endline "UNSAT");
+     | Solver.Sat _ ->
+       print_string "Result: SAT "
+     | Solver.Unsat _ -> print_string "Result: UNSAT ");
+    let stop = Unix.gettimeofday () in
+    Printf.printf "Execution time: %fs\n%!" (stop -. start);
     close_in ic;
   with _ -> close_in ic;
