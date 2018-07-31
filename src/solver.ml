54b3255246611ca48490447761fc42a481fe3cba
Yusuke Izawa <yuizalp@gmail.com>
2018-07-31 12:02:01 +0900

    add pure_literal_rule

--- src/solver.ml
+++ src/solver.ml
@@ -19,13 +19,31 @@ let rec apply_unit_rule (f : cnf) =
   | Some v -> assign f (List.hd v) |> apply_unit_rule
   | None -> f
 
+let is_pure (f : cnf) (v : literal) =
+  if v < 0 then false
+  else
+    List.map (List.find_opt (fun e -> e = (-v))) f
+    |> List.for_all (fun e -> e = None)
+
+let rec apply_pure_literal_rule (f : cnf) (v : literal) =
+  if v < 0 then None
+  else begin
+    if is_pure f v then Some (f |> List.filter (List.mem v))
+    else None
+  end
+
 let rec dpll (f : cnf) =
   match f |> apply_unit_rule with
   | [] -> true
   | new_f when List.exists List.is_empty new_f -> false
   | new_f ->
     let v = new_f |> List.hd |> List.hd in
-    dpll (assign new_f v) || dpll (assign new_f (-v))
+    match apply_pure_literal_rule new_f v with
+    | Some new_f' ->
+      let v' = new_f' |> List.hd |> List.hd in
+      dpll (assign new_f' v') || dpll (assign new_f' (-v'))
+    | None ->
+      dpll (assign new_f v) || dpll (assign new_f (-v))
 
 let solve (f : cnf) =
   if dpll f then Sat f
