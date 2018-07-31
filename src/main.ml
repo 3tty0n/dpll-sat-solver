8c0e3bc3b33b8e1a6b7fd3489ba4a3e54031b7dc
Yusuke Izawa <yuizalp@gmail.com>
2018-07-31 18:12:17 +0900

    fix

--- src/main.ml
+++ src/main.ml
@@ -10,10 +10,9 @@ let () =
       |> Cnf.literal_of_lists
       |> Solver.solve
      with
-     | Solver.Sat _ ->
-       print_string "Result: SAT "
-     | Solver.Unsat _ -> print_string "Result: UNSAT ");
+     | Solver.Sat _ -> print_endline "SAT"
+     | Solver.Unsat _ -> print_endline "UNSAT");
     let stop = Unix.gettimeofday () in
-    Printf.printf "Execution time: %fs\n%!" (stop -. start);
+    Printf.printf "Execution time (sec): %f\n%!" (stop -. start);
     close_in ic;
   with _ -> close_in ic;
